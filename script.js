90ec6e4539e8a0e05a496b4908a2817643255c37
Michael Sheng <you@example.com>
2021-01-06 00:11:51 -0500

    Chaned padding to section + general fixes

---
+++ script.js
@@ -0,0 +1,9 @@
+$(window).scroll(function() {     
+    var scroll = $(window).scrollTop();
+    if (scroll > 0) {
+        $("#header").addClass("active");
+    }
+    else {
+        $("#header").removeClass("active");
+    }
+});
